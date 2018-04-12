0a9a19bddc0c54939266cbd8499e1c13234a4cd1
rofleksey <evilmorty@yandex.ru>
2018-04-12 16:47:21 +0300

    Replaced CounterLatch sync blocks with sync methods

--- webcrawler/CountLatch.java
+++ webcrawler/CountLatch.java
@@ -1,36 +1,27 @@
 package ru.ifmo.rain.borisov.webcrawler;
 
 public class CountLatch {
-    private final Object lock = new Object();
-    int count = 0;
-    boolean finished = false;
+    private int count = 0;
+    private boolean finished = false;
 
-    public void waitUntilZero() throws InterruptedException {
-        synchronized (lock) {
-            while (!finished && count > 0) {
-                lock.wait();
-            }
+    public synchronized void waitUntilZero() throws InterruptedException {
+        while (!finished && count > 0) {
+            wait();
         }
     }
 
-    public void up() {
-        synchronized (lock) {
-            count++;
+    public synchronized void up() {
+        count++;
+    }
+
+    public synchronized void down() {
+        if (--count <= 0) {
+            notify();
         }
     }
 
-    public void down() {
-        synchronized (lock) {
-            if(--count <= 0) {
-                lock.notify();
-            }
-        }
-    }
-
-    public void finish() {
-        synchronized (lock) {
-            finished = true;
-            lock.notify();
-        }
+    public synchronized void finish() {
+        finished = true;
+        notify();
     }
 }
